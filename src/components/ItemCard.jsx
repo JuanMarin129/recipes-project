c6255ab5c82cc48809f7db022bdb5fad2efcd98c
Samuel Perez Besada <samusitesi@gmail.com>
2025-02-26 17:59:55 +0100

    Completado Dia 2

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,29 +1,31 @@
-import React from "react";
-
-function ItemCard() {
+function ItemCard(props) {
+    
   return (
-    <div
-      style={{
-        border: "1px solid black",
-        display: "flex",
-        flexDirection: "row",
-        textAlign: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <img
-        src="https://randomuser.me/api/portraits/men/44.jpg"
-        alt=""
-        width="200px"
-        height="200px"
-        style={{ textAlign: "center", margin: "5px" }}
-      />
+    <>
+      <div
+        style={{
+          border: "1px solid black",
+          display: "flex",
+          flexDirection: "row",
+          textAlign: "center",
+          justifyContent: "space-between",
+          backgroundColor: props.bgColor,
+        }}
+      >
+        <img
+          src={props.image}
+          alt=""
+          width="200px"
+          height="200px"
+          style={{ textAlign: "center", margin: "5px" }}
+        />
 
-      <p>Chocolate</p>
-      <p>Patata</p>
-      <p>Banana</p>
-      <p>Nueces</p>
-    </div>
+        <p>{props.name}</p>
+        <p>{props.calories}</p>
+        <p>{props.servings}</p>
+        <button onClick={() => props.handleDeleteItemCard(props.index)}>Delete</button>
+      </div>
+    </>
   );
 }
 
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,32 @@
-import ItemCard from "./ItemCard"
+import React, { useState } from "react";
+import ItemCard from "./ItemCard";
+import allRecipes from "../assets/recipes.json";
 
 function List() {
+    const [listRecipes, setListRecipes] = useState(allRecipes);
+    let bgColor;
+
+    const handleDeleteItemCard = (index) => {
+        let clone = listRecipes.slice(0);
+        clone.splice(index, 1);
+        setListRecipes(clone);
+      };
   return (
-    <div>
-        <ItemCard/>
-    </div>
-  )
+    <>
+      {listRecipes.map((eachRecipe, index) => {
+        eachRecipe.calories <= 300 ? (bgColor = "green") : (bgColor = "orange");
+        return <ItemCard 
+        name = {eachRecipe.name}
+        calories = {eachRecipe.calories}
+        servings = {eachRecipe.servings}
+        image = {eachRecipe.image}
+        index = {index}
+        bgColor = {bgColor}
+        handleDeleteItemCard = {handleDeleteItemCard}
+        />;
+      })}
+    </>
+  );
 }
 
-export default List
+export default List;
